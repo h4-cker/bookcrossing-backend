242f34e6db5706b193d6b3160ed2678fc8f57c3e
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-04-14 20:57:47 +0300

    use of cors has been added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoute from "./routes/auth.routes.js";
 
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 
