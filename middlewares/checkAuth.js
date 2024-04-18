0de3ac41e0bb06cb3093f71bc03376cf602e0149
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-04-18 18:41:50 +0300

    The authorization verification middleware has been added

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+      req.userId = decoded.userId;
+
+      next();
+    } catch (e) {
+      return res.status(403).json({
+        message: "Нет доступа",
+      });
+    }
+  } else {
+    return res.status(403).json({
+      message: "Нет доступа",
+    });
+  }
+};
