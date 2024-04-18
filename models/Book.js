72591f4f246b693266f1a3b7e23f206c242f2e78
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-04-18 20:24:52 +0300

    the user field has been removed from the book model

--- models/Book.js
+++ models/Book.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const BookSchema = new mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
     name: {
       type: String,
       required: true,
@@ -28,7 +23,7 @@ const BookSchema = new mongoose.Schema(
     },
     year: {
       type: Number,
-    }
+    },
   },
   {
     timestamps: true,
