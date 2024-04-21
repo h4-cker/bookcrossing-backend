9d5a97223bb2c40308d8572afc366f29ce304122
h4-cker <antonov.dima-a2017@yandex.ru>
2024-04-21 18:52:33 +0300

    Added ads CRUD controller

---
+++ controllers/AdController.js
@@ -0,0 +1,128 @@
+import AdModel from "../models/Ad";
+
+export const create = async (req, res) => {
+  try {
+    const ad = new AdModel({
+      user: req.userId,
+      book: req.body.book,
+      image: req.body.image,
+      description: req.body.description,
+      location: req.body.location,
+      type: req.body.type,
+    });
+
+    const post = await ad.save();
+
+    res.json(post);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Не удалось создать объявление',
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const adId = req.params.id;
+
+    await AdModel.findOne({_id: adId,},
+      (e, ad) => {
+        if (e) {
+          console.log(e);
+          return res.status(500).json({
+            message: 'Не удалось вернуть объявление',
+          });
+        }
+
+        if (!ad) {
+          return res.status(404).json({
+            message: 'Объявление не найдено',
+          });
+        }
+
+        res.json(ad);
+      },
+    ).populate(['user', 'book']);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить объявление',
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const ads = await AdModel.find().populate(['user', 'book']).exec();
+    res.json(ads);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить объявления',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const adId = req.params.id;
+
+    await AdModel.updateOne(
+      {
+        _id: adId,
+      },
+      {
+        user: req.userId,
+        book: req.body.book,
+        image: req.body.image,
+        description: req.body.description,
+        location: req.body.location,
+        type: req.body.type,
+      },
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Не удалось обновить объявление',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const adId = req.params.id;
+    AdModel.findOneAndDelete(
+      {
+        _id: adId,
+      },
+      (e, ad) => {
+        if (e) {
+          console.log(e);
+          return res.status(500).json({
+            message: 'Не удалось удалить объявление',
+          });
+        }
+
+        if (!ad) {
+          return res.status(404).json({
+            message: 'Объявление не найдено',
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      },
+    );
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Не удалось получить объявление',
+    });
+  }
+};
