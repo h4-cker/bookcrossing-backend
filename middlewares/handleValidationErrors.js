920346e723a07d0739e82586cb2c7029f9ab3ed6
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-04-22 18:47:23 +0300

    middleware for validation error handling has been added

---
+++ middlewares/handleValidationErrors.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+
+export default (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      message: "Некорректные данные при создании книги",
+    });
+  }
+
+  next();
+};
