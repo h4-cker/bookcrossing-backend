c9f57d363340175538be415331e9f29355c2febb
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-06-08 18:45:14 +0300

    cookieParser and Fingerprint have been added

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import adsRoutes from "./routes/ads.js";
+import cookieParser from "cookie-parser";
+import Fingerprint from "express-fingerprint";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
+
+app.use(
+  Fingerprint({
+    parameters: [Fingerprint.useragent, Fingerprint.acceptHeaders],
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/ads", adsRoutes);
