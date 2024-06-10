8592fb192d24fd90b693fa3a4a4b6dc39b28bdbc
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-06-11 01:02:43 +0300

    Uploading images has been added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import authRoutes from "./routes/auth.js";
 import adsRoutes from "./routes/ads.js";
 import cookieParser from "cookie-parser";
 import Fingerprint from "express-fingerprint";
+import multer from "multer";
+import checkAuth from "./middlewares/checkAuth.js";
 
 dotenv.config();
 
@@ -14,15 +16,32 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({ credentials: true }));
-
 app.use(
   Fingerprint({
     parameters: [Fingerprint.useragent, Fingerprint.acceptHeaders],
   })
 );
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `uploads/${req.file.originalname}`,
+  });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/ads", adsRoutes);
+app.use("/api/uploads", express.static("uploads"));
 
 async function connectDB() {
   await mongoose
