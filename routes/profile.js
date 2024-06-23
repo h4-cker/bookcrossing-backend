2e9cf0c7849b79a57efc4a36ae012e03385fad07
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-06-24 01:17:13 +0300

    paths in profile routes have been changed

--- routes/profile.js
+++ routes/profile.js
@@ -1,22 +1,15 @@
 import { Router } from "express";
 import checkAuth from "../middlewares/checkAuth.js";
-import handleValidationErrors from "../middlewares/handleValidationErrors.js";
 
 import * as ProfileController from "../controllers/ProfileController.js";
-import * as AdController from "../controllers/AdController.js";
 
 const router = new Router();
 
-router.post(
-  "/me/:id",
-  checkAuth,
-  handleValidationErrors,
-  ProfileController.getMe
-);
+router.post("/:id", checkAuth, ProfileController.getMe);
 
-router.patch("/:id", checkAuth, ProfileController.updateName);
-router.patch("/:id", checkAuth, ProfileController.updateEmail);
-router.patch("/:id", checkAuth, ProfileController.updatePassword);
+router.patch("/name", checkAuth, ProfileController.updateName);
+router.patch("/email", checkAuth, ProfileController.updateEmail);
+router.patch("/password", checkAuth, ProfileController.updatePassword);
 
 router.delete("/:id", checkAuth, ProfileController.removeMe);
 
