fd947a478cd5d0ea456fe52ad642ac0166730f3d
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2024-06-24 18:55:26 +0300

    the port receipt has been changed

--- index.js
+++ index.js
@@ -56,8 +56,10 @@ async function connectDB() {
 
 function startServer() {
   try {
-    app.listen(process.env.PORT, () =>
-      console.log(`Server has been started on port ${process.env.PORT}`)
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () =>
+      console.log(`Server has been started on port ${port}`)
     );
   } catch (error) {
     console.log("Server error: ", error.message);
